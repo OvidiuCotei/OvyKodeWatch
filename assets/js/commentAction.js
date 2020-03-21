21512b62ad22876b543d2f2cd2dd8fa00a7fd475
OvidiuCotei <ovidiu_metallica@yahoo.com>
2020-03-21 15:04:41 +0200

    comment insert

--- assets/js/commentAction.js
+++ assets/js/commentAction.js
@@ -5,8 +5,9 @@ function postComment(button, postedBy, videoId, replyTo, containerClass){
 
     if(commentText)
     {
-        $.post("ajax/postComment.php", {commentText: commentText, postedBy: postedBy, videoId: videoId, responseTo: replyTo}).done(function(data){
-            alert(data);
+        $.post("ajax/postComment.php", {commentText: commentText, postedBy: postedBy, 
+            videoId: videoId, responseTo: replyTo}).done(function(comment){
+            $("." + containerClass).prepend(comment);
         });
     }
     else
