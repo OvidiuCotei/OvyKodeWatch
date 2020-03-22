0d539eae2490559b294051908ba7267164aa7798
OvidiuCotei <ovidiu_metallica@yahoo.com>
2020-03-22 20:19:49 +0200

    more functionality

--- includes/classes/NavigationMenuProvider.php
+++ includes/classes/NavigationMenuProvider.php
@@ -1,27 +1,27 @@
 <?php
 class NavigationMenuProvider{
 
-    private $con, $userLoggedIn;
+    private $con, $userLoggedInObj;
 
-    public function __construct($con, $userLoggedIn)
+    public function __construct($con, $userLoggedInObj)
     {
         $this->con = $con;
-        $this->userLoggedIn = $userLoggedIn;
+        $this->userLoggedInObj = $userLoggedInObj;
     }
 
     public function create(){
         $menuHtml = $this->createNavItem("Home", "assets/images/icons/home.png", "index.php");
         $menuHtml .= $this->createNavItem("Trending", "assets/images/icons/trending.png", "trending.php");
         $menuHtml .= $this->createNavItem("Subscriptions", "assets/images/icons/subscriptions.png", "subscriptions.php");
-        $menuHtml .= $this->createNavItem("Liked Videos", "assets/images/icons/thumb-up.png", "linkedVideos.php");
+        $menuHtml .= $this->createNavItem("Liked Videos", "assets/images/icons/thumb-up.png", "likedVideos.php");
 
         if(User::isLoggedIn())
         {
             $menuHtml .= $this->createNavItem("Settings", "assets/images/icons/settings.png", "settings.php");
             $menuHtml .= $this->createNavItem("Log Out", "assets/images/icons/logout.png", "logout.php");
+            $menuHtml .= $this->createSubscriptionsSection();
         }
 
-        // Create Subscriptions section
         return "<div class='navigationItems'>
             $menuHtml
         </div>";
@@ -34,6 +34,19 @@ class NavigationMenuProvider{
             <span>$text</span>
         </a>
         </div>";
+    }
+
+    private function createSubscriptionsSection(){
+        $subscriptions = $this->userLoggedInObj->getSubscriptions();
+
+        $html = "<span class='heading'>Subscriptions</span>";
+
+        foreach($subscriptions as $sub)
+        {
+            $subUsername = $sub->getUsername();
+            $html .= $this->createNavItem($subUsername, $sub->getProfilePic(), "profile.php?username=$subUsername");
+        }
+        return $html;
     }
 }
 ?>
